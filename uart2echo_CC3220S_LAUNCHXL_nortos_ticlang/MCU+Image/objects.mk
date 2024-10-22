5735b4c945d8bf23751c79ff6bfe6ffdd471fbe0
Derek Spaulding <115181809+ogSlinger@users.noreply.github.com>
2024-10-22 07:25:39 -0500

    Delete uart2echo_CC3220S_LAUNCHXL_nortos_ticlang directory

--- uart2echo_CC3220S_LAUNCHXL_nortos_ticlang/MCU+Image/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -Wl,-lti_utils_build_linker.cmd.genlibs -Wl,-l"ti/devices/cc32xx/driverlib/ticlang/Release/driverlib.a" -Wl,-llibc.a
-
